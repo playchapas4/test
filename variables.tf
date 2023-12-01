1120618304f49e4216c56bc41d389fae34c4387c
Playchapas4 <playchapas4@hotmail.com>
2023-12-01 12:40:07 +0100

    update

--- variables.tf
+++ variables.tf
@@ -6,3 +6,19 @@ variable "etiquetas" {
   type        = map(string)
   description = "description"
 }
+
+variable client_id {
+    type = string
+}
+variable client_secret {
+    type = string
+}
+variable tenant_id {
+    type = string
+}
+variable subscription_id {
+    type = string
+}
+
+
+
